d2e67ec236c97f78fb75f388fe60048c655f944a
Khurshidbek-net <xurshidbekoktamaliyev651@gmail.com>
2025-06-14 12:01:37 +0500

    Deployment

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -13,10 +13,9 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Category')
-@Controller('api/category')
+@Controller('api/category')  
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
-
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
